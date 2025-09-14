a398b1fe1d917223eed57441b5981b92502d6437
Brian Lee <leebrian0908@gmail.com>
2025-09-14 22:59:07 +0800

    Initial Commit

---
+++ Homework2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework2
+//
+//  Created by Brian Lee on 9/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Homework2/Homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework2App.swift
+//  Homework2
+//
+//  Created by Brian Lee on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
